f0582008479adb29fbbb07d13dadeb05f9d90d62
Ryan Butler <ryan@techdrabble.com>
2019-02-20 17:45:40 -0600

    cleanup

--- ctxal-sdk/Public/remove-alconnector.ps1
+++ ctxal-sdk/Public/remove-alconnector.ps1
@@ -41,6 +41,7 @@ $headers = @{
 SOAPAction = "http://www.unidesk.com/DeletePlatformConnectorConfiguration";
 "Content-Type" = "text/xml; charset=utf-8";
 UNIDESK_TOKEN = $websession.token;
+
 }
 $url = "https://" + $websession.aplip + "/Unidesk.Web/API.asmx"
 if ($PSCmdlet.ShouldProcess("Removing $connid")) {
@@ -53,7 +54,7 @@ if ($PSCmdlet.ShouldProcess("Removing $connid")) {
 
   }
   else {
-    return $obj
+    #return $obj
     #Write-Verbose "WORKTICKET: $($obj.Envelope.Body.DeleteAppLayerRevisionsResponse.DeleteAppLayerRevisionsResult.WorkTicketId)"
     #return $obj.Envelope.Body.DeleteAppLayerRevisionsResponse.DeleteAppLayerRevisionsResult
   }
