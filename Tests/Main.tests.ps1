4359b547f35b6f3c8225e57922f4e0314aa046ff
Ryan Butler <ryan@techdrabble.com>
2019-04-01 20:44:48 -0500

    workaround for utf8

--- Tests/Main.tests.ps1
+++ Tests/Main.tests.ps1
@@ -9,7 +9,7 @@ Function Test-Encoding
         $stream = [System.IO.File]::OpenRead($path)
         $stream.Read($contents, 0, 3)|out-null
         $stream.Close()
-        ($contents[0] -eq 0x66 -and $contents[1] -eq 0x75 -and $contents[2] -eq 0x6E) -or ($contents[0] -eq 0x20 -and $contents[1] -eq 0x66 -and $contents[2] -eq 0x75) -or ($contents[0] -eq 0x24 -and $contents[1] -eq 0x50 -and $contents[2] -eq 0x75)
+        ($contents[0] -eq 0x66 -and $contents[1] -eq 0x75 -and $contents[2] -eq 0x6E) -or ($contents[0] -eq 0x20 -and $contents[1] -eq 0x66 -and $contents[2] -eq 0x75) -or ($contents[0] -eq 0x24 -and $contents[1] -eq 0x50 -and $contents[2] -eq 0x75) -or ($contents[0] -eq 70 -and $contents[1] -eq 117 -and $contents[2] -eq 110)
 
 }
 
