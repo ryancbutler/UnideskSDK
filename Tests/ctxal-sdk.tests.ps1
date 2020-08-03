42edeb52ea9c59dc35685d7726c374c24bb5cd7c
ryancbutler <ryancbutler@users.noreply.github.com>
2020-08-03 11:08:47 -0500

    better

--- Tests/ctxal-sdk.tests.ps1
+++ Tests/ctxal-sdk.tests.ps1
@@ -1,12 +1,9 @@
-$manifest = ".\ctxal-sdk\ctxal-sdk.psd1"
-$module = ".\ctxal-sdk\ctxal-sdk.psm1"
-
 Describe 'Module Metadata Validation' {      
         it 'Script fileinfo should be ok' {
-            {Test-ModuleManifest $manifest -ErrorAction Stop} | Should -Not -Throw
+            {Test-ModuleManifest ".\ctxal-sdk\ctxal-sdk.psd1" -ErrorAction Stop} | Should -Not -Throw
         }
         
         it 'Import module should be ok'{
-            {Import-Module $module -Force -ErrorAction Stop} | Should -Not -Throw
+            {Import-Module ".\ctxal-sdk\ctxal-sdk.psm1"-Force -ErrorAction Stop} | Should -Not -Throw
         }
 }
