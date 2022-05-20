c074eac2b816d5d2fc8ad9c646c1927ae51c674d
Khazaa <40522062+Khazaa@users.noreply.github.com>
2022-05-20 09:29:38 +0200

    Update New-ALPlatformLayer.ps1
    forgot to set a standardvalue

--- ctxal-sdk/Public/New-ALPlatformLayer.ps1
+++ ctxal-sdk/Public/New-ALPlatformLayer.ps1
@@ -49,7 +49,7 @@ function New-ALPlatformLayer {
   [cmdletbinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
   Param(
     [Parameter(Mandatory = $true)]$websession,    
-    [Parameter(Mandatory = $false)][string]$version,
+    [Parameter(Mandatory = $false)][string]$version = "1",
     [Parameter(Mandatory = $true)][string]$osrevid,
     [Parameter(Mandatory = $true)][string]$connectorid,
     [Parameter(Mandatory = $false)][string]$Description = "",
